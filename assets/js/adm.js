e53895aac55c3844aae5c0a0240fdae17ac9f846
Vitor Prange <12965@proway.treina>
2026-03-27 21:57:20 -0300

    mudança pra traslate no menu lateral

--- assets/js/adm.js
+++
@@ -1,9 +0,0 @@
-function abrirMenu() {
-    const menu = document.getElementById("menu");
-
-    if(menu.style.left === "0px"){
-        menu.style.left = "-400px";
-    } else {
-        menu.style.left = "0px";
-    }
-}
--- assets/js/autenticacao.js
+++ assets/js/autenticacao.js
@@ -1,7 +1,7 @@
 function verificarSenha(event) {
     const senha = prompt("Senha: ")
     if (senha == 9) {
-        window.location.href = "adm.html";
+        window.location.href = "dashboard.html";
     } else {
         alert("vaza daqui rato")
     }
